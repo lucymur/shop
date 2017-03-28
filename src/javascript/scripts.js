a4affd24e848ec80b578351f3d33fcdfb9033a16
aa <aa@norse.digital>
2017-03-28 23:06:52 +0300

    review

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -18,17 +18,8 @@
     });
 
 
-     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <script>
-  $( function() {
-    $( "#accordion" ).accordion();
-  } );
-  </script>
+    
 
-  <script>
-$( "#accordion" ).accordion();
-</script>
 
 
 })();
